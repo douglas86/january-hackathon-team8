520ca1973a6e7692380ac48a8617d09ec67bd54e
douglas <douglasmaxton@gmail.com>
2024-01-18 12:13:49 +0000

    created template directory variable

--- calculated_hackers/settings.py
+++ calculated_hackers/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
