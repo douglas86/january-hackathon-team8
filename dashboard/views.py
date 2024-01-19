2abe4719c253c6392293fa3a59e4dd8260651bee
douglas <douglasmaxton@gmail.com>
2024-01-19 16:08:13 +0000

    displays data from db to template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import UpcomingBill
 
 
 # Create your views here.
 class Dashboard(ListView):
-    queryset = 'none'
     template_name = 'dashboard/index.html'
+
+    def get_queryset(self):
+        return UpcomingBill.objects.all()
+
+    def get_context_data(self, **kwargs):
+        return {'bills': self.get_queryset()}
