25d4957f77f6b7c561a3519c998dc6af9a38a866
douglas <douglasmaxton@gmail.com>
2024-01-20 09:36:11 +0000

    changed dashboard url to home page

--- calculated_hackers/urls.py
+++ calculated_hackers/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls')),
+    path('', include('dashboard.urls')),
 ]
