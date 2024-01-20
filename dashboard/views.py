645c6c9cc4bb07c57112f192ff51846b863d86c1
douglas <douglasmaxton@gmail.com>
2024-01-20 23:11:51 +0000

    deleted all commented out code

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,29 +1,23 @@
 from django import forms
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from django.views.generic import ListView
 from django.views import View
 from .models import UpcomingBill
-from django.views.decorators.csrf import csrf_protect
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
 class Dashboard(View):
+    """
+    This view is responsible for sending and receiving from a database
+    """
     template_name = 'dashboard/index.html'
 
     def get(self, request, *args, **kwargs):
         bills = UpcomingBill.objects.filter(user=request.user)
         form = [EditDashboardForm(instance=bill) for bill in bills]
-        # form = EditDashboardForm(request.POST)
         my_list = zip(form, bills)
         context = {'my_list': my_list}
         return render(request, self.template_name, context)
-        # if self.request.user.is_authenticated:
-        #     return UpcomingBill.objects.filter(user=self.request.user)
-        # else:
-        #     return 'none'
 
     def post(self, request, pk, *args, **kwargs):
         bill = get_object_or_404(UpcomingBill, pk=pk)
@@ -31,7 +25,6 @@ class Dashboard(View):
         try:
             if form.is_valid():
                 new_bill = form.save(commit=False)
-                # new_bill.due_date = form.due_date.strftime('%Y-%m-%d')
                 new_bill.save()
                 return redirect('dashboard')
             else:
@@ -40,43 +33,9 @@ class Dashboard(View):
         except Exception as e:
             print(e)
 
-    # def get_context_data(self, **kwargs):
-    #     return {'bills': self.get_queryset()}
-
 
 class EditDashboardForm(forms.ModelForm):
     class Meta:
         model = UpcomingBill
         fields = '__all__'
         widgets = {'due_date': forms.DateInput(attrs={'type': 'date'})}
-
-
-class EditDashboard(View):
-    """
-    View to edit the dashboard of the titled document
-    """
-    # model = UpcomingBill
-    # form_class = EditDashboardForm
-    # template_name = 'dashboard/index.html'
-    # success_url = '/'
-
-    # def get(self, request, pk):
-    #     UpcomingBill.objects.get(pk=pk)
-    #     return JsonResponse()
-
-    # def post(self, request, pk, *args, **kwargs):
-    #     form = EditDashboardForm(request.POST)
-    #     # bill = get_object_or_404(UpcomingBill, pk=pk)
-    #     try:
-    #         if form.is_valid():
-    #             new_bill = form.save(commit=False)
-    #             new_bill.user = request.user
-    #             new_bill.title = form.title
-    #             new_bill.amount = form.amount
-    #             new_bill.due_date = form.due_date.strftime('%Y-%m-%d')
-    #             new_bill.save()
-    #             return redirect('')
-    #         else:
-    #             print(form.errors)
-    #     except Exception as e:
-    #         print(e)
